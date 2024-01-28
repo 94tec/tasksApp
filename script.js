02bf6205a49bcebd81f17a14cc04be3c84af7f80
Fixtone Kaloki <fixtone94tec@gmail.com>
2024-01-28 14:34:01 +0300

    Added Login Page

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function activeLink () {
 }
 list.forEach((item) =>
 item.addEventListener("click", activeLink));
+
+// Login Page js Script
+const container = document.getElementById('login-page');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', ()=>{
+    console.log('active');
+    container.classList.add("active");
+});
+loginBtn.addEventListener('click', ()=>{
+    console.log("clicked");
+    container.classList.remove("active");
+});
