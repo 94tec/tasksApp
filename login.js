454770a0f826d08e99ff1ee42a9638d9f92239a6
Fixtone Kaloki <fixtone94tec@gmail.com>
2024-02-12 02:44:36 +0300

    fixed Login Page

--- login.js
+++ login.js
@@ -1,3 +1,19 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+
+
+
+registerBtn.addEventListener('click', ()=>{
+    console.log('active');
+    container.classList.add("active");
+});
+loginBtn.addEventListener('click', ()=>{
+    container.classList.remove("active");
+});
+
+// firebase Set up
 import { 
     getAuth, 
     createUserWithEmailAndPassword,
